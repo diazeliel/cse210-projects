0751ac21192d5a5c1ecd9e82e70e02202bc9bb2a
diazeliel <cmadrid@byupathway.edu>
2025-10-13 23:06:47 -0600

    local fitness center

---
+++ week07/ExerciseTracking/Activity.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+
+// Base class
+abstract class Activity
+{
+    private DateTime _date;
+    private int _minutes;
+
+    public Activity(DateTime date, int minutes)
+    {
+        _date = date;
+        _minutes = minutes;
+    }
+
+    public DateTime GetDate() => _date;
+    public int GetMinutes() => _minutes;
+
+    public abstract double GetDistance(); // in km
+    public abstract double GetSpeed();    // in kph
+    public abstract double GetPace();     // in min/km
+
+    public virtual string GetSummary()
+    {
+        return $"{_date:dd MMM yyyy} {this.GetType().Name} ({_minutes} min): " +
+               $"Distance: {GetDistance():0.0} km, Speed: {GetSpeed():0.0} kph, Pace: {GetPace():0.00} min per km";
+    }
+}
